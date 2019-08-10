52989d1baae1315e73e1257f769ddf0824185213
Mark Rego <rego121@gmail.com>
2019-08-10 00:45:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,21 +11,29 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
+	base_hash = {
+		base: "hello"
+	}
+
+	# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
+	:railroads => "cool"
+
+	# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
+	railroads: ""
+
+	# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
